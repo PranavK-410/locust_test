99cdcf6ab348cbe6450f6ccca1c8e7be166fe9ab
Pranav Kulkarni <46425128+PranavK-410@users.noreply.github.com>
2023-08-06 18:19:04 +0530

    Create locustfile.py

---
+++ locustfile.py
@@ -0,0 +1,8 @@
+from locust import HttpUser, task
+            
+class User(HttpUser):
+    host= HttpUser.host
+    @task
+    def mainPage(self):
+        self.client.get("{}".format(self.host)
+        
